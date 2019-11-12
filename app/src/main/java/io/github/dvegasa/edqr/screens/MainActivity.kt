9fbc6e246dbf814831ef21663618b285f56ae70d
Ed <--->
2019-11-12 19:06:25 +0400

    [+] Permission handling

--- app/src/main/java/io/github/dvegasa/edqr/screens/MainActivity.kt
+++ app/src/main/java/io/github/dvegasa/edqr/screens/MainActivity.kt
@@ -1,8 +1,12 @@
 package io.github.dvegasa.edqr.screens
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.zxing.Result
 import io.github.dvegasa.edqr.InMemoryStorage
 import io.github.dvegasa.edqr.R
@@ -18,7 +22,37 @@ class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initQrScanner()
+        if (isCameraPermissionGranted()) {
+            initQrScanner()
+        } else {
+            askCameraPermission()
+        }
+    }
+
+    private fun isCameraPermissionGranted() =
+        ContextCompat.checkSelfPermission(
+            this, Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun askCameraPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 42)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            42 -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    initQrScanner()
+                } else {
+                    askCameraPermission()
+                }
+                return
+            }
+        }
     }
 
     private fun initQrScanner() {
